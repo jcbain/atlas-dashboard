a1464d8bbcf1c68b7df8c63ce24d410a31fdf158
Kyla Mempin <60671350+kmemps@users.noreply.github.com>
2021-05-19 16:13:10 -0600

    Update batch_jobs.sh

--- simulation_generator/descartes/batch_jobs.sh
+++ simulation_generator/descartes/batch_jobs.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-#SBATCH --account=def-yeaman
-#SBATCH --mem=5G
-#SBATCH --time=144:00:00
-#SBATCH --mail-user=james.bain@ucalgary.ca
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --array=1-32
