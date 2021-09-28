5623d2f26d1f7f942389b5570a3ae09bb3b5be3a
Misael Esperanzate <misael.esperanzate@hotmail.com>
2021-09-27 22:12:23 -0600

    changed path

---
+++ frontend/src/components/setup/Default.js
@@ -0,0 +1,81 @@
+import React from "react";
+import styled from "styled-components";
+import * as Style from "../../styles/SetupStyles.styles";
+
+const Default = ({ nextStep }) => {
+	return (
+		<section>
+			{" "}
+			<StepZeroWrapper>
+				<Style.SetupTitle>Default or Upload</Style.SetupTitle>
+				<DataContainer>
+					<SubTitle> Use default data </SubTitle>
+
+					<p>
+						{" "}
+						Continue to the data visualizer with the default data
+						and variables set.{" "}
+					</p>
+
+					<DefaultButton type="button">
+						{" "}
+						Continue to datavis{" "}
+					</DefaultButton>
+				</DataContainer>
+				<DataContainer>
+					<SubTitle> Upload files </SubTitle>
+
+					<p>
+						{" "}
+						Upload your own csv file / slim and parameters file and
+						select the variables to be visualized.
+					</p>
+
+					<UploadButton type="button" onClick={nextStep}>
+						{" "}
+						Continue to upload{" "}
+					</UploadButton>
+				</DataContainer>
+			</StepZeroWrapper>
+		</section>
+	);
+};
+
+export default Default;
+
+const StepZeroWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+
+const DataContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	border: 1px solid ${(props) => props.theme.chartCardOutline};
+	background-color: ${(props) => props.theme.chartCardBackground};
+	border-radius: 1rem;
+	padding: 3rem;
+	margin: 1rem;
+	max-width: 30rem;
+`;
+
+const SubTitle = styled.h4`
+	text-align: center;
+	text-decoration: underline;
+	margin-bottom: 1rem;
+`;
+
+const DefaultButton = styled.button`
+	border: none;
+	background-color: #f0ad4e;
+	padding: 0.5rem;
+	color: white;
+`;
+
+const UploadButton = styled.button`
+	border: none;
+	background-color: #169cb1;
+	padding: 0.5rem;
+	color: white;
+`;
--- frontend/src/components/setup/FileUpload.js
+++ frontend/src/components/setup/FileUpload.js
@@ -1,113 +1,134 @@
-import React, { useState } from 'react';
-import { Accordion, Card, Button } from 'react-bootstrap'
-import styled from 'styled-components';
+import React, { useState } from "react";
+import { Accordion, Card, Button } from "react-bootstrap";
+import styled from "styled-components";
 import * as Style from "../../styles/SetupStyles.styles";
 
-var FormData = require('form-data')
+var FormData = require("form-data");
 
-function FileUpload({ mutation }) {
-  	const [ invalidFile, setInvalidFile ] = useState(false);
-	const [ files, setFiles ] = useState({
-		'slimFile':'',
-		'jobsFile':'',
-		'dataFile':''
+function FileUpload({ mutation, nextStep, backStep }) {
+	const [invalidFile, setInvalidFile] = useState(false);
+	const [files, setFiles] = useState({
+		slimFile: "",
+		jobsFile: "",
+		dataFile: "",
 	});
 
 	const onSubmit = async (e) => {
 		e.preventDefault();
 		const formData = new FormData();
 
-		Object.keys(files).forEach(key => {
-			formData.append(key, files[key])
+		Object.keys(files).forEach((key) => {
+			formData.append(key, files[key]);
 		});
-		
+
 		mutation.mutate(formData);
-	}
+	};
 
 	const validateFile = (e) => {
-		let type = e.target.accept
-		let validExt = new RegExp(`${type}`, 'i')
-		let file = e.target.files[0]
+		let type = e.target.accept;
+		let validExt = new RegExp(`${type}`, "i");
+		let file = e.target.files[0];
 
 		if (!validExt.exec(file.name)) {
 			alert(`File needs to be ${type}`);
 			setInvalidFile(false);
 		} else {
-			setFiles(oldFile => ({
+			setFiles((oldFile) => ({
 				...oldFile,
-				[e.target.id]:file
+				[e.target.id]: file,
 			}));
 			setInvalidFile(true);
 		}
-	}
+	};
 
 	return (
-		<UploadDataWrapper onSubmit={onSubmit}>
-			<Style.SetupTitle> Upload Data </Style.SetupTitle>
-			<Accordion>
-				<Card>
-					<Card.Header>
-						<Accordion.Toggle as={Button} variant="link" eventKey="0">
-							Upload scripts
-						</Accordion.Toggle>
-					</Card.Header>
-					<Accordion.Collapse eventKey="0">
-						<Card.Body>
-							<label className="font-weight-bold">Slim script</label>
-							<input
-								type="file"
-								className="form-control-file"
-								id="slimFile"
-								accept=".slim"
-								onChange={validateFile}
-							/>
+		<section>
+			<UploadDataWrapper onSubmit={onSubmit}>
+				<Style.SetupTitle> Upload Data </Style.SetupTitle>
+				<Accordion>
+					<Card>
+						<Card.Header>
+							<Accordion.Toggle
+								as={Button}
+								variant="link"
+								eventKey="0"
+							>
+								Upload scripts
+							</Accordion.Toggle>
+						</Card.Header>
+						<Accordion.Collapse eventKey="0">
+							<Card.Body>
+								<label className="font-weight-bold">
+									Slim script
+								</label>
+								<input
+									type="file"
+									className="form-control-file"
+									id="slimFile"
+									accept=".slim"
+									onChange={validateFile}
+								/>
 
-							<label className="font-weight-bold mt-3">Parameters file</label>
-							<input
-								type="file"
-								className="form-control-file" 
-								id="jobsFile"
-								accept=".txt"
-								onChange={validateFile}
-							/>
-						</Card.Body>
-					</Accordion.Collapse>
-				</Card>
+								<label className="font-weight-bold mt-3">
+									Parameters file
+								</label>
+								<input
+									type="file"
+									className="form-control-file"
+									id="jobsFile"
+									accept=".txt"
+									onChange={validateFile}
+								/>
+							</Card.Body>
+						</Accordion.Collapse>
+					</Card>
 
-				<Card>
-					<Card.Header>
-						<Accordion.Toggle as={Button} variant="link" eventKey="1">
-							Upload data
-						</Accordion.Toggle>
-					</Card.Header>
-					<Accordion.Collapse eventKey="1">
-						<Card.Body>
-							<label className="font-weight-bold">Data</label>
-							<input
-								type="file"
-								className="form-control-file"
-								id="dataFile"
-								accept=".csv"
-								onChange={validateFile}
-							/>
-						</Card.Body>
-					</Accordion.Collapse>
-				</Card>
-			</Accordion>
+					<Card>
+						<Card.Header>
+							<Accordion.Toggle
+								as={Button}
+								variant="link"
+								eventKey="1"
+							>
+								Upload data
+							</Accordion.Toggle>
+						</Card.Header>
+						<Accordion.Collapse eventKey="1">
+							<Card.Body>
+								<label className="font-weight-bold">Data</label>
+								<input
+									type="file"
+									className="form-control-file"
+									id="dataFile"
+									accept=".csv"
+									onChange={validateFile}
+								/>
+							</Card.Body>
+						</Accordion.Collapse>
+					</Card>
+				</Accordion>
 
-			<Style.StyledInputButton
-				type="submit"
-				value="Upload"
-				disabled={!invalidFile}/>
-		</UploadDataWrapper>
-	)
+				<Style.StyledInputButton
+					type="submit"
+					value="Upload"
+					disabled={!invalidFile}
+					onClick={nextStep}
+				/>
+				<Style.StyledInputButton
+					type="button"
+					value="back"
+					onClick={backStep}
+				/>
+			</UploadDataWrapper>
+		</section>
+	);
 }
 
 export default FileUpload;
 
 const UploadDataWrapper = styled.form`
-    border: 1px solid ${props => props.theme.chartCardOutline};
-    background-color: ${props => props.theme.chartCardBackground};
-    border-radius: 1rem;
-    padding: 2rem;
-`
+	border: 1px solid ${(props) => props.theme.chartCardOutline};
+	background-color: ${(props) => props.theme.chartCardBackground};
+	border-radius: 1rem;
+	padding: 2rem;
+`;
