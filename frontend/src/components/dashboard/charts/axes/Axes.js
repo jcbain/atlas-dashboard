d5dc4b3296f48692b8d4f43998f08ef8b1210526
Misael Esperanzate <misael.esperanzate@hotmail.com>
2021-09-27 21:13:06 -0600

    Update Axes.js

--- frontend/src/components/dashboard/charts/axes/Axes.js
+++ frontend/src/components/dashboard/charts/axes/Axes.js
@@ -1,63 +1,72 @@
-import { XAxis, YAxis } from '.';
-import Brush from '../brush/Brush';
-import * as d3 from 'd3';
-import { TickText } from './AxisParts';
+import { XAxis, YAxis } from ".";
+import Brush from "../brush/Brush";
+import * as d3 from "d3";
+import { TickText } from "./AxisParts";
 
 const margin = 40;
 
-const variables = JSON.parse(sessionStorage.getItem('visuals'));
+const variables = JSON.parse(sessionStorage.getItem("visuals"));
 
 const Axes = ({ axes, dimensions, addBrush, selection, setSelection }) => {
-    let yAxisLabel= <TickText
-                        fontSize={17}
-                        textAnchor="start"
-                        transform={`translate(2, ${dimensions.height/2}) rotate(90)`}
-                    >
-                        { variables.y }
-                    </TickText>
-    
-    let xAxisLabel= <TickText
-                        fontSize={17}
-                        textAnchor="start"
-                        transform={`translate(${(dimensions.width/2)-margin}, ${dimensions.height + margin/2 + 15})`}
-                    >
-                        { variables.x }
-                    </TickText>
+	let yAxisLabel;
+	let xAxisLabel;
+	if (variables !== null) {
+		yAxisLabel = (
+			<TickText
+				fontSize={17}
+				textAnchor="start"
+				transform={`translate(2, ${dimensions.height / 2}) rotate(90)`}
+			>
+				{variables.y}
+			</TickText>
+		);
 
-    const xScale = d3.scaleLinear()
-        .domain([axes.minX, axes.maxX])
-        .range([margin, dimensions.width-margin]);
+		xAxisLabel = (
+			<TickText
+				fontSize={17}
+				textAnchor="start"
+				transform={`translate(${dimensions.width / 2 - margin}, ${
+					dimensions.height + margin / 2 + 15
+				})`}
+			>
+				{variables.x}
+			</TickText>
+		);
+	}
 
-    const yScale = d3.scaleLinear()
-        .domain([axes.minY, axes.maxY])
-        .range([dimensions.height, margin]);
+	const xScale = d3
+		.scaleLinear()
+		.domain([axes.minX, axes.maxX])
+		.range([margin, dimensions.width - margin]);
 
-    return (
-        <svg width="100%" height="100%" style={{ position: 'relative', overflow: 'visible' }}>
-            <YAxis
-                yScale={yScale}
-                margin={margin}
-            />
-            { !addBrush && yAxisLabel }
+	const yScale = d3
+		.scaleLinear()
+		.domain([axes.minY, axes.maxY])
+		.range([dimensions.height, margin]);
 
-            <XAxis
-                xScale={xScale}
-                dimensions={dimensions}
-            />
-            { addBrush && xAxisLabel}
+	return (
+		<svg
+			width="100%"
+			height="100%"
+			style={{ position: "relative", overflow: "visible" }}
+		>
+			<YAxis yScale={yScale} margin={margin} />
+			{!addBrush && yAxisLabel}
 
-            { addBrush && 
-                <Brush
-                    xScale={xScale}
-                    dimensions={dimensions}
-                    margin={margin}
-                    selection={selection}
-                    setSelection={setSelection}
-                />
-            }
-        </svg>
+			<XAxis xScale={xScale} dimensions={dimensions} />
+			{addBrush && xAxisLabel}
 
-    )
-}
+			{addBrush && (
+				<Brush
+					xScale={xScale}
+					dimensions={dimensions}
+					margin={margin}
+					selection={selection}
+					setSelection={setSelection}
+				/>
+			)}
+		</svg>
+	);
+};
 
-export { Axes }
+export { Axes };
